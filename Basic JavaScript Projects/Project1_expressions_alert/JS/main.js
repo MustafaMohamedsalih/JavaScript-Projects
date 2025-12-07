296e22b83c645272de3470b2e8d21836a3a7d5e4
MustafaMohamedsalih <MustafaAbdelrahmanAbdelmoti.Mohamedsalih@my-aolcc.com>
2025-12-07 18:36:40 -0500

    Re-added Basic JavaScript Projects folder with Project1_expressions_alert assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+// --- Basic JavaScript Projects Assignment ---
+// This script demonstrates alerts, document writing, variables, concatenation, and expressions.
+
+// Display a message using window.alert()
+window.alert("Welcome! This is my Basic JavaScript Project.");
+
+// Create two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the two variables and display them
+document.write(Sent1 + Sent2);
+
+// Write a simple arithmetic expression
+var sum = 10 + 5; // Expression: addition
+document.write("<br>The result of 10 + 5 is: " + sum);
